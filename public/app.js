7467a5e842add61352973c40f61a69a42be75d86
praitayini <praitayini@gmail.com>
2017-10-18 11:58:46 -0400

    hj

---
+++ public/app.js
@@ -0,0 +1,27 @@
+var config = {
+    apiKey: "AIzaSyC9Vwq3aBS8y-PvutBiRhOmO0BywRbnV2M",
+    authDomain: "firestore-1da30.firebaseapp.com",
+    databaseURL: "https://firestore-1da30.firebaseio.com",
+    projectId: "firestore-1da30",
+    storageBucket: "firestore-1da30.appspot.com",
+    messagingSenderId: "11860285726"
+};
+firebase.initlaizeApp(config);
+var firestore = firebase.firestore();
+
+const docRef = firestore.doc("samples/americaData");
+const outputHeader = document.querySelector("#Americaoutput");
+const inputTextField = document.querySelector("#latestAmericastatus");
+const saveButton = document.querySelector("#savebutton");
+
+saveButton.addEventListener("click",function(){
+    const textToSave = inputTextField.value;
+    console.log("I am going to save" + textToSave + "to Firestore");
+    docRef.set({
+        hotDogStatus: textToSave
+    }).then(function(){
+        console.log("Status saved!");
+    }).catch(function(error){
+        console.log("Got an error:",error);
+    });
+})
